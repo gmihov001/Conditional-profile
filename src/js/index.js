afa64c58b9f86cf128561fed26a6f46bbf3d7660
George Mihov <gmihov001@gmail.com>
2020-06-02 01:14:58 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -40,7 +40,7 @@ function render(variables = {}) {
             <li><a href="${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
             <li><a href="${variables.github}"><i class="fa fa-github"></i></a></li>
             <li><a href="${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
