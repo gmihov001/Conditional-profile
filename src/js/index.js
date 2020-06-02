9b481539ef89ad4d0b3668f3739f977b099f73e0
George Mihov <gmihov001@gmail.com>
2020-06-02 01:13:57 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -37,9 +37,9 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city + ", " + variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
